4535aad556c1b21de62912381ecbf5a996621de8
Jacob L. <jlewis.cw743@gmail.com>
2024-09-30 12:53:35 -0400

    Optimization tweaks
    (unfinished)

--- assets/scenes/enemies/slime.gd
+++ assets/scenes/enemies/slime.gd
@@ -18,40 +18,33 @@ var health = 5
 
 func _ready():
 	set_scale(Vector3(5,5,5))
+	globs.total_slimes += 1
 
 func _physics_process(delta):
-	if health <= 0:
-		globs.total_slimes -= 1
-		var i = str_to_var(HUD.get_node("Score/scoreValue").text) + 100
-		HUD.get_node("Score/scoreValue").text = var_to_str(i)
-		var particles = $attackParticles
-		$attackParticles.emitting = true
-		$attackParticles/dead.play()
-		remove_child(particles)
-		particles.position = position
-		get_parent().add_child(particles)
-		call_deferred("queue_free")
-	var collision = move_and_collide(velocity * delta)
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	if(collision):
-		if collision.get_collider().name == "player":
-			player.take_damage()
-			rotation.y += 180
-			moves_left = randi_range(50,100)
-		if $frontRay.is_colliding():
-			if $frontRay.get_collider().name == "GridMap":
-				rotation.y += delta
-				moves_left += delta
-	if(moves_left > 0 and $slimeAnimation.current_animation != "damaged"):
-		var angle = self.get_rotation().y
-		position += (Vector3(sin(angle),0, cos(angle)) * 10 ) * (delta * SPEED)
-		moves_left -= 1
-	else:
-		if ($actTimer.is_stopped()):
-			$actTimer.start()
-			
-	move_and_slide()
+	match(health):
+		0:
+			die()
+		_:
+			var collision = move_and_collide(velocity * delta)
+			if not is_on_floor():
+				velocity.y -= gravity * delta
+			if(collision):
+				if collision.get_collider().name == "player":
+					player.take_damage()
+					rotation.y += 180
+					moves_left = randi_range(50,100)
+				if $frontRay.is_colliding() and $frontRay.get_collider().name == "GridMap":
+					rotation.y += delta
+					moves_left += delta
+			if(moves_left > 0 and $slimeAnimation.current_animation != "damaged"):
+				var angle = self.get_rotation().y
+				position += (Vector3(sin(angle),0, cos(angle)) * 10 ) * (delta * SPEED)
+				moves_left -= 1
+			else:
+				if ($actTimer.is_stopped()):
+					$actTimer.start()
+					
+			move_and_slide()
 
 
 func _on_act_timer_timeout():
@@ -102,3 +95,15 @@ func _on_slime_animation_animation_finished(anim_name):
 
 func _on_attack_particles_finished():
 	queue_free()
+
+func die():
+	globs.total_slimes -= 1
+	var i = str_to_var(HUD.get_node("Score/scoreValue").text) + 100
+	HUD.get_node("Score/scoreValue").text = var_to_str(i)
+	var particles = $attackParticles
+	$attackParticles.emitting = true
+	$attackParticles/dead.play()
+	remove_child(particles)
+	particles.position = position
+	get_parent().add_child(particles)
+	call_deferred("queue_free")
--- assets/scenes/enemies/spawner.gd
+++ assets/scenes/enemies/spawner.gd
@@ -2,6 +2,7 @@ extends MeshInstance3D
 
 @onready var SLIME = preload("res://assets/scenes/enemies/slime.tscn")
 @onready var globs = get_tree().root.get_node("/root/Globs")
+var blocked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +13,17 @@ func _process(_delta):
 	pass
 
 func _on_timer_timeout():
-	if globs.total_slimes < 20:
-		globs.total_slimes += 1
+	if globs.total_slimes < 20 and blocked == false:
 		var enemy = SLIME.instantiate()
 		get_tree().root.get_node("mainLevel").add_child(enemy)
 		enemy.global_transform = global_transform
 		enemy.set_scale(Vector3(5,5,5))
 		$Timer.wait_time = randi_range(3,10)
+
+func _on_area_3d_body_entered(body):
+	if("slime" in body.name or "CharacterBody3D" in body.name):
+		blocked == true
+
+func _on_area_3d_body_exited(body):
+	if("slime" in body.name or "CharacterBody3D" in body.name):
+		blocked == false
