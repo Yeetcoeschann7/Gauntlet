ab3de07feddba28acb3c3d8179e30e19dd6068a3
Ructoon <jlewis.cw743@gmail.com>
2024-10-03 09:50:39 -0400

    Hud Optimized

--- assets/scenes/UI/HUD/HUD.gd
+++ assets/scenes/UI/HUD/HUD.gd
@@ -1,19 +1,28 @@
 extends Control
 
 @onready var globs = get_tree().root.get_node("/root/Globs")
+@onready var effect:AudioEffectLowPassFilter = AudioServer.get_bus_effect(AudioServer.get_bus_index("Music"), 0)
 var dead = false
 enum {SEVEN_TWENTY, NINE_HUNDRED, TEN_EIGHTY}
 var resolution = SEVEN_TWENTY
+const NORM_RES = 1
+const NORM_CUTOFF = 9999
+const MUFFLED_RES = 0.2
+const MUFFLED_CUTOFF = 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	effect.cutoff_hz = NORM_CUTOFF
+	effect.resonance = NORM_RES
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	$gameOver.visible = false
 	$pause.visible = false
+	$ColorRect.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$ColorRect/healthBar.value = globs.player_health
+	$ColorRect/healthBar/heathLabel.text = var_to_str(roundi($ColorRect/healthBar.value))
 	$ColorRect/hiScore/hiScoreValue.text = var_to_str(int(globs.highscore))
 	$ColorRect/Score/scoreValue.text = var_to_str(globs.score)
 	if get_tree().paused != true:
@@ -23,20 +32,20 @@ func _process(_delta):
 		else:
 			$ColorRect/Timer/timeLeft.set("theme_override_colors/font_color", Color(1,1,1))
 	if Input.is_action_just_pressed("pause") and $gameOver.visible == false and get_tree().paused == false:
-		$pause/Score/scoreValue.text = $ColorRect/Score/scoreValue.text
+		effect.cutoff_hz = MUFFLED_CUTOFF
+		effect.resonance = MUFFLED_RES
 		$pause.visible = true
-		$pause/Score.visible = false
 		$pause/resume.visible = true
 		get_tree().paused = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		$pause/resume.grab_focus()
 		
 func game_over():
+	effect.cutoff_hz = MUFFLED_CUTOFF
+	effect.resonance = MUFFLED_RES
 	if globs.score > globs.highscore:
 		globs.highscore = globs.score
-	$gameOver/Score/scoreValue.text = $Score/scoreValue.text
 	$gameOver.visible = true
-	$gameOver/Score.visible = true
 	$gameOver/retry.visible = true
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -47,11 +56,15 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_resume_pressed():
+	effect.cutoff_hz = NORM_CUTOFF
+	effect.resonance = NORM_RES
 	get_tree().paused = false
 	$pause.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _on_retry_pressed():
+	effect.cutoff_hz = NORM_CUTOFF
+	effect.resonance = NORM_RES
 	get_tree().paused = false
 	$gameOver.visible = false
 	globs.total_slimes = 0
